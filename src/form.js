1383bbe8d37dff424834bb8d0c3194a47dab8f9b
p-a-racine <pierrealexandreracine@gmail.com>
2022-04-04 14:02:01 +0200

    feat : nettoyage js

--- src/form.js
+++ src/form.js
@@ -3,21 +3,21 @@
     // width to the value defined here, but the height will be
     // calculated based on the aspect ratio of the input stream.
 
-    var width = 320;    // We will scale the photo width to this
-    var height = 0;     // This will be computed based on the input stream
+    const width = 320;    // We will scale the photo width to this
+    let height = 0;     // This will be computed based on the input stream
 
     // |streaming| indicates whether or not we're currently streaming
     // video from the camera. Obviously, we start at false.
 
-    var streaming = false;
+    let streaming = false;
 
     // The various HTML elements we need to configure or control. These
     // will be set by the startup() function.
 
-    var video = null;
-    var canvas = null;
-    var photo = null;
-    var startbutton = null;
+    let video = null;
+    let canvas = null;
+    let photo = null;
+    let startbutton = null;
 
     function showViewLiveResultButton() {
         if (window.self !== window.top) {
@@ -81,11 +81,11 @@
     // captured.
 
     function clearphoto() {
-        var context = canvas.getContext('2d');
+        const context = canvas.getContext('2d');
         context.fillStyle = "#AAA";
         context.fillRect(0, 0, canvas.width, canvas.height);
 
-        var data = canvas.toDataURL('image/png');
+        const data = canvas.toDataURL('image/png');
         photo.setAttribute('src', data);
     }
 
@@ -96,13 +96,13 @@
     // other changes before drawing it.
 
     function takepicture() {
-        var context = canvas.getContext('2d');
+        const context = canvas.getContext('2d');
         if (width && height) {
             canvas.width = width;
             canvas.height = height;
             context.drawImage(video, 0, 0, width, height);
 
-            var data = canvas.toDataURL('image/png');
+            const data = canvas.toDataURL('image/png');
             photo.setAttribute('src', data);
         } else {
             clearphoto();
@@ -113,9 +113,3 @@
     // once loading is complete.
     window.addEventListener('load', startup, false);
 })();
-
-
-function envoiDonnees(){
-    var data = canvas.toDataURL('image/jpeg');
-    console.log()
-}
